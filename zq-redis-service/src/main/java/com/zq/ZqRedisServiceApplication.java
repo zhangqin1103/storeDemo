26360e99cf78d1b4c0d2c45895e94be438dd84f9
zhangq <156043800@qq.com>
2018-12-26 16:24:05 +0800

    redis-service

--- zq-redis-service/src/main/java/com/zq/ZqRedisServiceApplication.java
+++ zq-redis-service/src/main/java/com/zq/ZqRedisServiceApplication.java
@@ -2,8 +2,10 @@ package com.zq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ZqRedisServiceApplication {
 
     public static void main(String[] args) {
