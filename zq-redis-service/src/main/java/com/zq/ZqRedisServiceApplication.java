57a945d944d5b03faf71650ad56114371f8a5ad2
zhangq <156043800@qq.com>
2018-12-26 16:15:14 +0800

    redis-service

--- zq-redis-service/src/main/java/com/zq/ZqRedisServiceApplication.java
+++ zq-redis-service/src/main/java/com/zq/ZqRedisServiceApplication.java
@@ -2,10 +2,8 @@ package com.zq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ZqRedisServiceApplication {
 
     public static void main(String[] args) {
